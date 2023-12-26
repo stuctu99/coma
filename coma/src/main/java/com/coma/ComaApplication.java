7eec3997a9c3f67eb3bd997dfae9167ca86db15f
goodee <goodee@host.docker.internal>
2023-12-26 14:11:16 +0900

    ignore 추가 Scripts/ .dbeaver/ src/main/resources/

---
+++ coma/src/main/java/com/coma/ComaApplication.java
@@ -0,0 +1,13 @@
+package com.coma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComaApplication.class, args);
+	}
+
+}
---
+++ coma/src/test/java/com/coma/ComaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
