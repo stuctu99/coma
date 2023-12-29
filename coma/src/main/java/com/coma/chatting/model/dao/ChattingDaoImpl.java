b14b554149aa8b0805f7a6051878c52a25138c44
iam0chan <devyc7@gmail.com>
2023-12-30 02:25:08 +0900

    YC) 메신저-채팅 직원 불러오기 및 생성 채팅방 리스트 출력 (2023/12/30 02:24)

--- coma/src/main/java/com/coma/chatting/model/dao/ChattingDaoImpl.java
+++ coma/src/main/java/com/coma/chatting/model/dao/ChattingDaoImpl.java
@@ -10,18 +10,29 @@ import com.coma.model.dto.Emp;
 
 @Repository
 public class ChattingDaoImpl implements ChattingDao {
-
+//	select
 	@Override
-	public List<Emp> selectEmpAll(SqlSession session) {
+	public List<Emp> selectEmpListAll(SqlSession session) {
 		// TODO Auto-generated method stub
-		return session.selectList("employee.selectEmpAll");
+		return session.selectList("emp.selectEmpAll");
+	}
+	
+	@Override
+	public List<ChattingRoom> selectRoomList(SqlSession session) {
+		// TODO Auto-generated method stub
+		return session.selectList("chatting.selectRoomList");
 	}
 
+//	insert
 	@Override
 	public int insertChattingRoom(SqlSession session, ChattingRoom room) {
 		// TODO Auto-generated method stub
 		System.out.println(room);
 		return session.insert("chatting.insertCreateRoom",room);
 	}
+	
+//	update
+	
+//	delete
 
 }
