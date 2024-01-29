e94e73ad23198ee9ff24e1b8fbb9e077392d0c0e
LeeKyuHong <LeeKyuHong@host.docker.internal>
2024-01-29 09:28:46 +0900

    kh_Exception 페이지 리턴 주소값 수정

--- coma/src/main/java/com/coma/security/CustomErrorController.java
+++ coma/src/main/java/com/coma/security/CustomErrorController.java
@@ -4,22 +4,24 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class CustomErrorController implements ErrorController{
 	
 	@GetMapping("/error-page/403")
-	public String errorPage403() {
-		return "errors/403";
+	public String errorPage403(HttpServletRequest request) {
+		return request.getContextPath()+"/errors/403";
 	}
 	
 	@GetMapping("/error-page/404")
-    public String errorPage404() {
-        return "errors/404";
+    public String errorPage404(HttpServletRequest request) {
+        return request.getContextPath()+"/errors/404";
     }
 
 	@GetMapping("/error-page/500")
-    public String errorPage500() {
-        return "errors/500";
+    public String errorPage500(HttpServletRequest request) {
+        return request.getContextPath()+"/errors/500";
     }
 
 	
