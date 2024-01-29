727e6dbb98269b08448a53f70b6abf288155935e
LeeKyuHong <LeeKyuHong@host.docker.internal>
2024-01-29 11:41:26 +0900

    kh_예외처리 수정

--- coma/src/main/java/com/coma/security/WebServerCustomizer.java
+++ coma/src/main/java/com/coma/security/WebServerCustomizer.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@Component
-public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+//@Component
+//public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
-	@Override
-	public void customize(ConfigurableWebServerFactory factory) {
-		ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/GDJ71_COMA_final/error-page/404");
-        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/GDJ71_COMA_final/error-page/500");
-        ErrorPage errorPageRuntimeException = new ErrorPage(RuntimeException.class, "/GDJ71_COMA_final/error-page/500");
-        factory.addErrorPages(errorPage404, errorPage500, errorPageRuntimeException);
-	}
+//	@Override
+//	public void customize(ConfigurableWebServerFactory factory) {
+//		ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/GDJ71_COMA_final/error-page/404");
+//        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/GDJ71_COMA_final/error-page/500");
+//        ErrorPage errorPageRuntimeException = new ErrorPage(RuntimeException.class, "/GDJ71_COMA_final/error-page/500");
+//        factory.addErrorPages(errorPage404, errorPage500, errorPageRuntimeException);
+//	}
 	
-}
+//}
